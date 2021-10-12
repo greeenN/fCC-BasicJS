1a9119a6daff733033ff7b40f9458c0571dcc7e2
greeenN <nikgrishin1999@mail.ru>
2021-10-12 23:57:53 +0300

    38

--- script.js
+++ script.js
@@ -23,3 +23,10 @@ var myArray = [['Yo', 7]];*/
 
 /*var myArray = [50,60,70];
 var myData = myArray[0];*/
+
+// 38
+
+/*// Setup
+var myArray = [18,64,99];
+myArray[0] = 45;
+// Only change code below this line*/

a54e10eaf437d9d08422329fec2142a10091c7fe
greeenN <nikgrishin1999@mail.ru>
2021-10-12 23:54:53 +0300

    all commits in one file

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-var myNoun = "dog";
+/*34*/
+
+/*var myNoun = "dog";
 var myAdjective = "big";
 var myVerb = "ran";
 var myAdverb = "quickly";
 
 // Only change code below this line
 var wordBlanks = myAdjective + ' ' + myNoun + ' ' + myVerb + ' ' + myAdverb ; // Change this line
-// Only change code above this line
+// Only change code above this line*/
+
+// 35
+
+/*// Only change code below this line
+var myArray = ['Hello', 5];*/
+
+// 36
+
+/*// Only change code below this line
+var myArray = [['Yo', 7]];*/
+
+// 37
+
+/*var myArray = [50,60,70];
+var myData = myArray[0];*/

6993399e48dc276b77d210b14a8ff190b45c8df5
greeenN <nikgrishin1999@mail.ru>
2021-10-13 17:14:35 +0300

    41

--- script.js
+++ script.js
@@ -36,3 +36,55 @@ myArray[0] = 45;
 /*var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
 
 var myData = myArray[2][1];*/
+
+// 40
+
+/*// Setup
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+// Only change code below this line*/
+
+// 41
+
+/*// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.pop();*/
+
+// 42
+
+
+
+// 43
+
+
+
+// 44
+
+
+
+// 45
+
+
+
+// 46
+
+
+
+// 47
+
+
+
+// 48
+
+
+
+// 49
+
+
+
+// 50
+
+
+

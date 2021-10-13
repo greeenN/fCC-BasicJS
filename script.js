e9cb4ff312a52a92bf3d09f00c6e3cea2a1dbf73
greeenN <nikgrishin1999@mail.ru>
2021-10-13 13:00:34 +0300

    39

--- script.js
+++ script.js
@@ -30,3 +30,9 @@ var myData = myArray[0];*/
 var myArray = [18,64,99];
 myArray[0] = 45;
 // Only change code below this line*/
+
+// 39
+
+/*var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+var myData = myArray[2][1];*/

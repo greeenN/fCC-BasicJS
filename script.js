4bba636841e32002fa4be0dcaffeedc89eeca162
greeenN <nikgrishin1999@mail.ru>
2021-10-14 17:07:55 +0300

    58

--- script.js
+++ script.js
@@ -169,3 +169,26 @@ var answer = timesFive(5);
 
 }*/
 
+// 57
+
+/*// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);*/
+
+// 58
+
+/*// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);*/

ef07f81b06be0065b22251fa724f40d93d86a5b7
greeenN <nikgrishin1999@mail.ru>
2021-10-14 17:32:03 +0300

    63

--- script.js
+++ script.js
@@ -192,3 +192,71 @@ function testStrict(val) {
 }
 
 testStrict(10);*/
+
+// 59
+
+/*// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, 10);*/
+
+// 60
+
+/*// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);*/
+
+// 61
+
+/*// Setup
+function testStrictNotEqual(val) {
+  if (val !== 17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);*/
+
+// 62
+
+/*function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);*/
+
+// 63
+
+/*function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);*/

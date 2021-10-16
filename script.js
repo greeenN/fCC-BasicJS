e2b3bc8eada1f1609be55545e4a6960264736c3d
greeenN <nikgrishin1999@mail.ru>
2021-10-16 16:56:22 +0300

    added 70

--- script.js
+++ script.js
@@ -323,3 +323,52 @@ testLogicalAnd(10);*/
 }
 
 testLogicalOr(15);*/
+
+// 68
+
+/*function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);*/
+
+// 69
+
+/*function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+
+  return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);*/
+
+// 70
+
+/*function orderMyLogic(val) {
+   if (val < 5) {
+    return "Less than 5";
+  } if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);*/

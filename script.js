b92abba583f2bb086bc42a960d4ad147324ae1e8
greeenN <nikgrishin1999@mail.ru>
2021-10-16 20:03:22 +0300

    added 81

--- script.js
+++ script.js
@@ -564,3 +564,43 @@ if (a < 0 || b < 0) {
 }
 
 abTest(2,2);*/
+
+// 79
+
+/*var myDog = {
+// Only change code below this line
+  name: 'Doggy',
+  legs: 4,
+  tails: 1,
+  friends: ['Catty', 'Mousy']
+
+// Only change code above this line
+};*/
+
+// 80
+
+// Setup
+/*var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line*/
+
+// 81
+
+// Setup
+/*var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj['an entree'];   // Change this line
+var drinkValue = testObj['the drink'];    // Change this line*/

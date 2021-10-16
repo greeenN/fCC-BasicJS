46126d96f0df7d29427f1a8825c1e6d9f72b47b9
greeenN <nikgrishin1999@mail.ru>
2021-10-16 19:43:32 +0300

    added 75

--- script.js
+++ script.js
@@ -421,3 +421,88 @@ if ( strokes == 1) {
 }
 
 golfScore(5, 4);*/
+
+
+// 73
+
+/*function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+switch(val) {
+  case 1:
+    answer = "alpha";
+    break;
+  case 2:
+    answer = "beta";
+    break;
+  case 3:
+    answer = "gamma";
+    break;
+  case 4:
+    answer = "delta";
+    break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);*/
+
+// 74
+
+/*function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
+switch(val) {
+  case "a":
+    answer = "apple";
+    break;
+  case "b":
+    answer = "bird";
+    break;
+  case "c":
+    answer = "cat";
+    break;
+  default:
+    answer = "stuff";
+    break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);*/
+
+// 75
+
+/*function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+switch(val) {
+  case 1:
+  case 2:
+  case 3:
+    answer = "Low";
+    break;
+  case 4:
+  case 5:
+  case 6:
+    answer = "Mid";
+    break;
+  case 7:
+  case 8:
+  case 9:
+    answer = "High";
+    break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);*/

46d81456bf7debd63c2684a6d7704d33d0d63a38
greeenN <nikgrishin1999@mail.ru>
2021-10-18 20:54:40 +0300

    added 95

--- script.js
+++ script.js
@@ -837,3 +837,12 @@ for (var i = 1; i < 6; i++) {
   myArray.push(i);
 }*/
 // Only change code below this line
+
+// 95
+
+// Setup
+/*var myArray = [];
+for(var i = 1; i <= 9; i += 2) {
+  myArray.push(i);
+}*/
+// Only change code below this line

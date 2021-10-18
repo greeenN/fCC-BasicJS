fc912067da50e0a2f8b2c009c37c2ff1c8b9a182
greeenN <nikgrishin1999@mail.ru>
2021-10-19 01:10:41 +0300

    added 104

--- script.js
+++ script.js
@@ -971,3 +971,11 @@ lookUpProfile("Akira", "likes");*/
 
   return Math.floor(Math.random() * 10);
 }*/
+
+// 104
+
+/*function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return Math.floor(Math.random() * (myMax - myMin +1) + myMin);
+  // Only change code above this line
+}*/

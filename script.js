1a6c843cbc23a8690825310d1c5af40be9fca4d4
greeenN <nikgrishin1999@mail.ru>
2021-10-18 20:29:41 +0300

    added 91

--- script.js
+++ script.js
@@ -792,3 +792,26 @@ phoneticLookup("charlie");*/
 };
 
 var gloveBoxContents = myStorage.car.inside["glove box"];*/
+
+// 92 
+
+/*var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+var secondTree = myPlants[1].list[1];*/

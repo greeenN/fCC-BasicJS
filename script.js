bc1f33b4073ea6f8b247f44d743ed8af237c7de2
greeenN <nikgrishin1999@mail.ru>
2021-10-29 16:00:14 +0300

    "added 110"

--- script.js
+++ script.js
@@ -1019,3 +1019,13 @@ checkSign(10);*/
 /*
 let catName = "Oliver";
 let catSound = "Meow!";*/
+
+// 110
+
+// Only change code below this line
+/*const FCC = "freeCodeCamp";
+let fact = "is cool!";
+// Only change code above this line
+
+fact = "is awesome!";
+console.log(FCC, fact);*/
